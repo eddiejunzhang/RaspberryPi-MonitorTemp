ff545db5eead723c27ea043a92a652890d1b94f1
EddieJunZhang <junzhang@aliyun.com>
2018-06-09 19:54:21 +0800

    cycle

--- dht11test02.py
+++ dht11test02.py
@@ -9,76 +9,83 @@ GPIO.setmode(GPIO.BCM)
 
 time.sleep(1)
 
-GPIO.setup(channel, GPIO.OUT)
-GPIO.output(channel, GPIO.LOW)
-time.sleep(0.02)
-GPIO.output(channel, GPIO.HIGH)
-GPIO.setup(channel, GPIO.IN)
+while True:
+  GPIO.setup(channel, GPIO.OUT)
+  GPIO.output(channel, GPIO.LOW)
+  time.sleep(0.02)
+  GPIO.output(channel, GPIO.HIGH)
+  GPIO.setup(channel, GPIO.IN)
 
-while GPIO.input(channel) == GPIO.LOW:
-  continue
-while GPIO.input(channel) == GPIO.HIGH:
-  continue
-
-while j < 40:
-  k = 0
   while GPIO.input(channel) == GPIO.LOW:
     continue
   while GPIO.input(channel) == GPIO.HIGH:
-    k += 1
-    if k > 100:
-      break
-  if k < 8:
-    data.append(0)
+    continue
+
+  j = 0
+  while j < 40:
+    k = 0
+    while GPIO.input(channel) == GPIO.LOW:
+      continue
+    while GPIO.input(channel) == GPIO.HIGH:
+      k += 1
+      if k > 100:
+        break
+    if k < 8:
+      data.append(0)
+    else:
+      data.append(1)
+
+    j += 1
+
+  print "sensor is working."
+  print data
+
+  humidity_bit = data[0:8]
+  humidity_point_bit = data[8:16]
+  temperature_bit = data[16:24]
+  temperature_point_bit = data[24:32]
+  check_bit = data[32:40]
+
+  humidity = 0
+  humidity_point = 0
+  temperature = 0
+  temperature_point = 0
+  check = 0
+
+  for i in range(8):
+    humidity += humidity_bit[i] * 2 ** (7-i)
+    humidity_point += humidity_point_bit[i] * 2 ** (7-i)
+    temperature += temperature_bit[i] * 2 ** (7-i)
+    temperature_point += temperature_point_bit[i] * 2 ** (7-i)
+    check += check_bit[i] * 2 ** (7-i)
+
+  tmp = humidity + humidity_point + temperature + temperature_point
+
+  status="Low"
+  if temperature>26:
+    status="High"
+  if temperature>27:
+    status="Danger"
+
+  if check == tmp:
+    print "temperature :", temperature, "*C, humidity :", humidity, "%"
+    with open("log.txt","a") as f:
+      line = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) + ' temperature :' + str(temperature)+ '*C, humidity :' + str(humidity)+ '%'+ ' Status=' + status + '\n'
+      f.write(line)
+    with open('cur.txt','w') as f:
+      f.write(line)
   else:
-    data.append(1)
-
-  j += 1
-
-print "sensor is working."
-print data
-
-humidity_bit = data[0:8]
-humidity_point_bit = data[8:16]
-temperature_bit = data[16:24]
-temperature_point_bit = data[24:32]
-check_bit = data[32:40]
-
-humidity = 0
-humidity_point = 0
-temperature = 0
-temperature_point = 0
-check = 0
-
-for i in range(8):
-  humidity += humidity_bit[i] * 2 ** (7-i)
-  humidity_point += humidity_point_bit[i] * 2 ** (7-i)
-  temperature += temperature_bit[i] * 2 ** (7-i)
-  temperature_point += temperature_point_bit[i] * 2 ** (7-i)
-  check += check_bit[i] * 2 ** (7-i)
-
-tmp = humidity + humidity_point + temperature + temperature_point
-
-status="Low"
-if temperature>26:
-  status="High"
-if temperature>27:
-  status="Danger"
-
-if check == tmp:
-  print "temperature :", temperature, "*C, humidity :", humidity, "%"
-  with open("log.txt","a") as f:
-    line = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) + ' temperature :' + str(temperature)+ '*C, humidity :' + str(humidity)+ '%'+ ' Status=' + status + '\n'
-    f.write(line)
-  with open('cur.txt','w') as f:
-    f.write(line)
-else:
-  print "wrong"
-  print "temperature :", temperature, "*C, humidity :", humidity, "% check :", check, ", tmp :", tmp
-  with open("log.txt","a") as f:
-    line = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) + ' NODATA'+ '\n' 
-    f.write(line)
-  with open('cur.txt','w') as f:
-    f.write(line)
+    print "wrong"
+    print "temperature :", temperature, "*C, humidity :", humidity, "% check :", check, ", tmp :", tmp
+    with open("log.txt","a") as f:
+      line = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) + ' NODATA'+ '\n' 
+      f.write(line)
+    with open('cur.txt','w') as f:
+      f.write(line)
+#empty array data
+  while len(data)>0:
+    data.pop()
     
+  time.sleep(10)
+
 GPIO.cleanup()
